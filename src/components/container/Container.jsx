5abae5cdc412a234bcf3140f0f50e01334fb4304
Serejenka <mikheevs11@gmail.com>
2020-04-28 20:25:03 +0300

    Separated routing creation logic from Container

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,72 +1,11 @@
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import React from "react";
-import RegisterContainer from "../register/RegisterContainer";
-import LoginContainer from "../login/LoginContainer";
-import Profile from "../profile/Profile";
-import SupportContainer from "../support/SupportContainer";
-import Confirm from "../confirm/Confirm";
-import { withRouter, Route, Switch } from "react-router-dom";
+import { withRouter, Switch } from "react-router-dom";
 import "./Container.css";
-import PrivateRoute from "../common/privateroute/PrivateRoute";
-import SandboxContainer from "../sandbox/SandboxContainer";
-import Root from "../root/Root";
+import { getAllRoutes } from "../../js/services/RoutesService";
 
 const Container = (props) => {
-  const routesData = [
-    {
-      path: "/sandbox",
-      isExact: false,
-      render: () => <SandboxContainer />,
-    },
-    {
-      path: "/login",
-      isExact: true,
-      render: () => <LoginContainer />,
-    },
-    {
-      path: "/register",
-      isExact: true,
-      render: () => <RegisterContainer />,
-    },
-    {
-      path: "/confirm",
-      isExact: false,
-      render: () => <Confirm />,
-    },
-    {
-      path: "/",
-      isExact: true,
-      render: () => <Root />,
-    },
-  ];
-  const privateRoutesData = [
-    {
-      path: "/profile/:userId",
-      isExact: true,
-      render: () => <Profile />,
-    },
-    {
-      path: "/support",
-      isExact: true,
-      render: () => <SupportContainer />,
-    },
-  ];
-
-  const routesComponents = routesData.map((elem) => (
-    <Route
-      key={elem.path}
-      path={elem.path}
-      exact={elem.isExact}
-      component={elem.component || elem.render}
-    />
-  ));
-  const privateRoutesComponents = privateRoutesData.map((elem) => (
-    <PrivateRoute key={elem.path} path={elem.path} exact={elem.isExact}>
-      {elem.render}
-    </PrivateRoute>
-  ));
-
-  const routes = [...routesComponents, ...privateRoutesComponents];
+  const routes = getAllRoutes();
 
   return (
     <TransitionGroup component={null}>
---
+++ src/js/services/RoutesService.js
@@ -0,0 +1,70 @@
+import React from "react";
+import PrivateRoute from "../../components/common/privateroute/PrivateRoute";
+import SandboxContainer from "../../components/sandbox/SandboxContainer";
+import Root from "../../components/root/Root";
+import RegisterContainer from "../../components/register/RegisterContainer";
+import LoginContainer from "../../components/login/LoginContainer";
+import Profile from "../../components/profile/Profile";
+import SupportContainer from "../../components/support/SupportContainer";
+import Confirm from "../../components/confirm/Confirm";
+import { Route } from "react-router-dom";
+
+const routesData = [
+  {
+    path: "/sandbox",
+    isExact: false,
+    render: () => <SandboxContainer />,
+  },
+  {
+    path: "/login",
+    isExact: true,
+    render: () => <LoginContainer />,
+  },
+  {
+    path: "/register",
+    isExact: true,
+    render: () => <RegisterContainer />,
+  },
+  {
+    path: "/confirm",
+    isExact: false,
+    render: () => <Confirm />,
+  },
+  {
+    path: "/",
+    isExact: true,
+    render: () => <Root />,
+  },
+];
+const privateRoutesData = [
+  {
+    path: "/profile/:userId",
+    isExact: true,
+    render: () => <Profile />,
+  },
+  {
+    path: "/support",
+    isExact: true,
+    render: () => <SupportContainer />,
+  },
+];
+
+const routesComponents = routesData.map((elem) => (
+  <Route
+    key={elem.path}
+    path={elem.path}
+    exact={elem.isExact}
+    component={elem.component || elem.render}
+  />
+));
+const privateRoutesComponents = privateRoutesData.map((elem) => (
+  <PrivateRoute key={elem.path} path={elem.path} exact={elem.isExact}>
+    {elem.render}
+  </PrivateRoute>
+));
+
+const routes = [...routesComponents, ...privateRoutesComponents];
+
+export const getAllRoutes = () => {
+  return routes;
+};
